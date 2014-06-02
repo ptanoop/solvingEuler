66911e22831b98ec74dfe7de3ed909fb4470b810
unknown <Anoop.PT@DMC1168L.emaratech.ae>
2014-06-02 12:34:00 +0400

    Committing Third Problem

---
+++ largePrimeFactor.py
@@ -0,0 +1,32 @@
+# Problem Three
+"""
+
+	Author	:	ANOOP P T
+	Date	:	01/06/2014
+ 
+	Problem	:	The prime factors of 13195 are 5, 7, 13 and 29.
+
+	What is the largest prime factor of the number 600851475143 ?
+	
+	
+"""
+import math
+
+def isprime(num):
+	is_prime = True;
+	sqr = int(math.sqrt(num))
+	for n in range (2, sqr):
+		if(num%n==0):
+			is_prime = False
+			break
+	return is_prime
+
+
+num = 600851475143 
+sqr = int(math.sqrt(num) + 1)
+largePrimeFactor = 2
+for n in range(2,sqr):
+	if num % n == 0 and isprime(n):
+		largePrimeFactor = n
+print largePrimeFactor
+
