8085fed465338c5285a31f148a84cf3d88946318
EMARATECH\Anoop.PT <anoop.pt@emaratech.ae>
2017-01-25 16:38:16 +0400

    Adding golang code - problem one

--- golang_codes/10001st_prime.go
+++ golang_codes/10001st_prime.go
@@ -1,1 +1,30 @@
-"import \"fmt\"" 
+/*
+
+Problem One
+
+Author	:	ANOOP YASEEN P T
+Date	:	01/06/2014
+
+Problem	:	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 0; i < 1000; i++{
+		if i % 3 == 0 || i % 5 == 0 {
+			sum = sum + i
+		}
+	}
+	fmt.Println(sum)
+
+
+
+
+
+}
